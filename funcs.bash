b2b62a90dce7dd72bf7109b2c44eb518288614f4
Zhaowei Fang <zwfangio@gmail.com>
2020-06-30 09:56:17 +0800

    add a function to append PATH

--- funcs.bash
+++ funcs.bash
@@ -2,3 +2,12 @@ function IS_EXISTS_IN_ENV_PATH() {
     echo ${PATH} | /usr/bin/tr ':' '\n' | grep -q -E "^${1}$"
 }
 
+function APPEND_ENV_PATH() {
+    while test $# -gt 0; do
+        if ! IS_EXISTS_IN_ENV_PATH ${1} ; then
+	    export PATH="${1}:${PATH}";
+        fi
+        shift
+    done
+}
+
